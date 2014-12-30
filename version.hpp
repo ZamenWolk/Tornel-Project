baa9b8449213f275180a894e1afd2ecad7ea24b5
ZamenWolk <martin.canivez@hotmail.fr>
2014-12-30 02:54:14 +0100

    Updated version

--- version.hpp
+++ version.hpp
@@ -35,11 +35,11 @@ struct VersionNumber
 namespace Version
 {
     static const char          STATUS[]             = "Alpha";
-    static const long          MAJOR                = 1;
-    static const long          MINOR                = 13;
-    static const long          PATCH                = 1;
+    static const long          MAJOR                = 2;
+    static const long          MINOR                = 0;
+    static const long          PATCH                = 0;
     static const VersionNumber VERSION(STATUS, MAJOR, MINOR, PATCH);
-    static const char          FULLVERSION_STRING[] = "a1.13.1";
+    static const char          FULLVERSION_STRING[] = "a2.0.0";
 }
 
 sf::Packet &operator<<(sf::Packet &packet, const VersionNumber &version);
